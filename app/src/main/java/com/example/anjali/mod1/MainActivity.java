3b9ab53a7c823fbea24ef17192dfddaec653c7f4
geethanjalibaskar <33482066+geethanjalibaskar@users.noreply.github.com>
2018-02-01 21:42:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/anjali/mod1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.anjali.mod1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+     Button btn1;
+     ImageView img1;
+     Animation frombottom, fromtop;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = findViewById(R.id.btn1);
+        ImageView img1 = findViewById(R.id.img1);
+
+        frombottom = AnimationUtils.loadAnimation(this,R.anim.frombottom);
+        fromtop = AnimationUtils.loadAnimation(this,R.anim.fromtop);
+
+        btn1.setAnimation(frombottom);
+        img1.setAnimation(fromtop);
+    }
+}
